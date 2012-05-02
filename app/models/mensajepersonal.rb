fc6528f59a9f52585e7371d0b1a9a42861bb23f7
Lucas Padovan <lucaslockon@gmail.com>
2012-05-01 21:44:28 -0300

    First commit

---
+++ app/models/mensajepersonal.rb
@@ -0,0 +1,8 @@
+class Mensajepersonal < ActiveRecord::Base
+  belongs_to :remitente, :class_name => 'User'
+  belongs_to :destinatario, :class_name => 'User'
+  belongs_to :hilo, :class_name => 'Mensajepersonal'
+
+  has_many :respuestas, :class_name => 'Mensajepersonal'
+
+end
